305e607f9428a033112bd95b0905254c92962b13
Danushan <danushanr@yaratechnologies.com>
2024-10-19 22:31:01 +0530

    add provider package

--- lib/nextScreen.dart
+++ lib/nextScreen.dart
@@ -1,4 +1,9 @@
+import 'dart:core';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class Nextscreen extends StatefulWidget {
   final List<int> num;
@@ -14,6 +19,46 @@ class Nextscreen extends StatefulWidget {
 class _NextscreenState extends State<Nextscreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          int latenum = widget.num.last;
+          setState(() {
+            widget.num.add(latenum + 1);
+          });
+        },
+        child: const Row(
+          children: [
+            Icon(Icons.add),
+            Text("add"),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text("Provider App"),
+      ),
+      body: SizedBox(
+        child: Column(
+          children: [
+            Text(widget.num.last.toString(),
+                style: const TextStyle(fontSize: 25)),
+            Container(
+              height: 200,
+              width: double.maxFinite,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: widget.num.length,
+                itemBuilder: (context, index) {
+                  return Text(
+                    widget.num[index].toString(),
+                    style: const TextStyle(fontSize: 25),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
