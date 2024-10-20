a3f70047e2d89910ee1a42584dbff6afdade2f98
Danushan <danushanr@yaratechnologies.com>
2024-10-20 13:53:07 +0530

    start new assignment

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,21 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
           ),
           // routes: {},
-          home: ProviderHome(),
+          home: Home(),
         ));
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
